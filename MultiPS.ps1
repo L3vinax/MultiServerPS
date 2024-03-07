b2dd31db886e85a6d44d80fd4a845933041b3658
L3vinax <111082932+L3vinax@users.noreply.github.com>
2024-03-07 11:29:14 -0800

    Update MultiPS.ps1

--- MultiPS.ps1
+++ MultiPS.ps1
@@ -1,7 +1,15 @@
+#Import Active Directory module to allow Get-ADComputers to work.  
+#If running on a server, the RSAT-AD-Powershell windows feature may need to be installed, simply uncomment line 3.
+#Install-WindowsFeature RSAT-AD-Powershell
 Import-Module -Name ActiveDirectory
 
+#Get list of server names from Active Directory. This returns only the name of the Server.  
+#Further filtering could be done to focus on a specific OU
 $servers = Get-ADComputer -Filter * | select-object -expandproperty Name
 
+#Loop through the returned servers variable and run a command or script on each machine.
+#Uncomment either line 17 or 18 depending on need.
+#If a machine is not available, it will write the name to the Unavailable-Servers.txt file
 ForEach ($Server in $Servers)
 {
   Try
